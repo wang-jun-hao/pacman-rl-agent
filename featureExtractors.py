27411a631da5ad1f19bf8b59199476154233b12b
Jun Hao <wang_jun_hao@yahoo.com>
2021-07-16 02:41:53 +0800

    add source code

---
+++ featureExtractors.py
@@ -0,0 +1,199 @@
+# featureExtractors.py
+# --------------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+"Feature extractors for Pacman game states"
+
+from game import Directions, Actions
+import util
+
+class FeatureExtractor:
+    def getFeatures(self, state, action):
+        """
+          Returns a dict from features to counts
+          Usually, the count will just be 1.0 for
+          indicator functions.
+        """
+        util.raiseNotDefined()
+
+class IdentityExtractor(FeatureExtractor):
+    def getFeatures(self, state, action):
+        feats = util.Counter()
+        feats[(state,action)] = 1.0
+        return feats
+
+class CoordinateExtractor(FeatureExtractor):
+    def getFeatures(self, state, action):
+        feats = util.Counter()
+        feats[state] = 1.0
+        feats['x=%d' % state[0]] = 1.0
+        feats['y=%d' % state[0]] = 1.0
+        feats['action=%s' % action] = 1.0
+        return feats
+
+def closestFood(pos, food, walls):
+    """
+    closestFood -- this is similar to the function that we have
+    worked on in the search project; here its all in one place
+    """
+    fringe = [(pos[0], pos[1], 0)]
+    expanded = set()
+    while fringe:
+        pos_x, pos_y, dist = fringe.pop(0)
+        if (pos_x, pos_y) in expanded:
+            continue
+        expanded.add((pos_x, pos_y))
+        # if we find a food at this location then exit
+        if food[pos_x][pos_y]:
+            return dist
+        # otherwise spread out from the location to its neighbours
+        nbrs = Actions.getLegalNeighbors((pos_x, pos_y), walls)
+        for nbr_x, nbr_y in nbrs:
+            fringe.append((nbr_x, nbr_y, dist+1))
+    # no food found
+    return None
+
+class SimpleExtractor(FeatureExtractor):
+    """
+    Returns simple features for a basic reflex Pacman:
+    - whether food will be eaten
+    - how far away the next food is
+    - whether a ghost collision is imminent
+    - whether a ghost is one step away
+    """
+
+    def getFeatures(self, state, action):
+        # extract the grid of food and wall locations and get the ghost locations
+        food = state.getFood()
+        walls = state.getWalls()
+        ghosts = state.getGhostPositions()
+
+        features = util.Counter()
+
+        features["bias"] = 1.0
+
+        # compute the location of pacman after he takes the action
+        x, y = state.getPacmanPosition()
+        dx, dy = Actions.directionToVector(action)
+        next_x, next_y = int(x + dx), int(y + dy)
+
+        # count the number of ghosts 1-step away
+        features["#-of-ghosts-1-step-away"] = sum((next_x, next_y) in Actions.getLegalNeighbors(g, walls) for g in ghosts)
+
+        # if there is no danger of ghosts then add the food feature
+        if not features["#-of-ghosts-1-step-away"] and food[next_x][next_y]:
+            features["eats-food"] = 1.0
+
+        dist = closestFood((next_x, next_y), food, walls)
+        if dist is not None:
+            # make the distance a number less than one otherwise the update
+            # will diverge wildly
+            features["closest-food"] = float(dist) / (walls.width * walls.height)
+        features.divideAll(10.0)
+        return features
+
+
+class NewExtractor(FeatureExtractor):
+    """
+    Design you own feature extractor here. You may define other helper functions you find necessary.
+    """
+    """
+        Returns simple features for a basic reflex Pacman:
+        - whether food will be eaten
+        - how far away the next food is
+        - whether a ghost collision is imminent
+        - whether a ghost is one step away
+    """
+
+    def getFeatures(self, state, action):
+        # extract the grid of food and wall locations and get the ghost locations
+        food = state.getFood()
+        walls = state.getWalls()
+        ghosts = state.getGhostPositions()
+
+        features = util.Counter()
+
+        features["bias"] = 1.0
+
+        # compute the location of pacman after he takes the action
+        x, y = state.getPacmanPosition()
+        dx, dy = Actions.directionToVector(action)
+        next_x, next_y = int(x + dx), int(y + dy)
+
+        # features to eat ghost or avoid ghost
+        isSafe = True
+        hasScaredGhost = False
+        for i, g in enumerate(state.getGhostStates()):
+            if g.scaredTimer > 2:
+                hasScaredGhost = True
+
+        allScared = True
+        for i, g in enumerate(state.getGhostStates()):
+            if g.scaredTimer <= 2:
+                allScared = False
+
+        if allScared:
+            features["all-scared"] = 1.0
+
+        # If there are 2 or more ghosts
+        if hasScaredGhost:
+            for i, g in enumerate(state.getGhostStates()):
+                if g.scaredTimer > 2:
+                    features["is-ghost{}-2-steps-away".format(i)] = 0
+                    gx, gy = g.getPosition()
+                    closestGhost = util.manhattanDistance((next_x, next_y), (gx, gy))
+                    features["eats-ghost{}".format(i)] = float(closestGhost) / (walls.width + walls.height)
+                else:
+                    step1 = Actions.getLegalNeighbors(g.getPosition(), walls)
+                    isGhostNext = 0
+
+                    step2 = []
+                    for g_1 in step1:
+                        if (next_x, next_y) in Actions.getLegalNeighbors(g_1, walls):
+                            isGhostNext = 1.0
+                        step2 += Actions.getLegalNeighbors(g_1, walls)
+
+                    features["is-ghost{}-2-steps-away".format(i)] = isGhostNext
+                    if isGhostNext:
+                        isSafe = False
+            if isSafe:
+                features["eats-food"] = 1.0
+        else:
+            neighbouring_ghosts = 0
+            step1 = []
+            for g in ghosts:
+                step1 += Actions.getLegalNeighbors(g, walls)
+            for step in step1:
+                if step == (next_x, next_y):
+                    neighbouring_ghosts += 1
+
+            pacman_positions = Actions.getLegalNeighbors((next_x, next_y), walls)
+            step2 = []
+            for s in step1:
+                step2 += Actions.getLegalNeighbors(s, walls)
+            for step in step2:
+                for p in pacman_positions:
+                    if step == p:
+                        neighbouring_ghosts += 1
+            features["#-of-ghosts-2-steps-away"] = neighbouring_ghosts
+
+            if not features["#-of-ghosts-2-steps-away"]:
+                features["eats-food"] = 1.0
+
+        dist = closestFood((next_x, next_y), food, walls)
+        if dist is not None:
+            # make the distance a number less than one otherwise the update
+            # will diverge wildly
+            features["closest-food"] = float(dist) / (walls.width * walls.height)
+        features.divideAll(10.0)
+        return features
---
+++ qlearningAgents.py
@@ -0,0 +1,248 @@
+# qlearningAgents.py
+# ------------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+from game import *
+from learningAgents import ReinforcementAgent
+from featureExtractors import *
+
+import random,util,math
+
+class QLearningAgent(ReinforcementAgent):
+    """
+      Q-Learning Agent
+
+      Functions you should fill in:
+        - computeValueFromQValues
+        - computeActionFromQValues
+        - getQValue
+        - getAction
+        - update
+
+      Instance variables you have access to
+        - self.epsilon (exploration prob)
+        - self.alpha (learning rate)
+        - self.discount (discount rate)
+
+      Functions you should use
+        - self.getLegalActions(state)
+          which returns legal actions for a state
+    """
+    def __init__(self, **args):
+        "You can initialize Q-values here..."
+        ReinforcementAgent.__init__(self, **args)
+
+        "*** YOUR CODE HERE ***"
+        self.qMap = {}
+
+    def getQValue(self, state, action):
+        """
+          Returns Q(state,action)
+          Should return 0.0 if we have never seen a state
+          or the Q node value otherwise
+        """
+        "*** YOUR CODE HERE ***"
+
+        return self.qMap.get((state, action), 0.0)
+
+
+    def computeValueFromQValues(self, state):
+        """
+          Returns max_action Q(state,action)
+          where the max is over legal actions.  Note that if
+          there are no legal actions, which is the case at the
+          terminal state, you should return a value of 0.0.
+        """
+        "*** YOUR CODE HERE ***"
+        actions = self.getLegalActions(state);
+
+        if len(actions) == 0:
+            return 0.0
+
+        currArgMax = actions[0]
+        currMaxQValue = self.getQValue(state, currArgMax)
+
+        for a in actions:
+            qValue = self.getQValue(state, a)
+
+            if qValue > currMaxQValue:
+                currMaxQValue = qValue
+
+        return currMaxQValue
+
+
+    def computeActionFromQValues(self, state):
+        """
+          Compute the best action to take in a state.  Note that if there
+          are no legal actions, which is the case at the terminal state,
+          you should return None.
+        """
+        "*** YOUR CODE HERE ***"
+        actions = self.getLegalActions(state);
+
+        if len(actions) == 0:
+            return None
+
+        currArgMax = actions[0]
+        currArgMaxList = [currArgMax]
+        currMaxQValue = self.getQValue(state, currArgMax)
+        for a in actions:
+            qValue = self.getQValue(state, a)
+
+            if qValue > currMaxQValue:
+                currArgMaxList = [a]
+                currMaxQValue = qValue
+            elif qValue == currMaxQValue:
+                currArgMaxList.append(a)
+
+        return random.choice(currArgMaxList)
+
+
+    def getAction(self, state):
+        """
+          Compute the action to take in the current state.  With
+          probability self.epsilon, we should take a random action and
+          take the best policy action otherwise.  Note that if there are
+          no legal actions, which is the case at the terminal state, you
+          should choose None as the action.
+
+          HINT: You might want to use util.flipCoin(prob)
+          HINT: To pick randomly from a list, use random.choice(list)
+        """
+        # Pick Action
+        actions = self.getLegalActions(state)
+        "*** YOUR CODE HERE ***"
+
+        if len(actions) == 0:
+            return None
+
+        bestAction = self.computeActionFromQValues(state)
+
+        if util.flipCoin(self.epsilon):
+            # explore
+            return random.choice(actions)
+        else:
+            # don't explore
+            return bestAction
+
+
+    def update(self, state, action, nextState, reward):
+        """
+          The parent class calls this to observe a
+          state = action => nextState and reward transition.
+          You should do your Q-Value update here
+
+          NOTE: You should never call this function,
+          it will be called on your behalf
+        """
+        "*** YOUR CODE HERE ***"
+
+        currQValue = self.getQValue(state, action)
+
+        lectureFunction = reward + (self.discount * self.computeValueFromQValues(nextState))
+
+        self.qMap[(state, action)] = self.alpha * lectureFunction + (1 - self.alpha) * currQValue
+
+
+    def getPolicy(self, state):
+        return self.computeActionFromQValues(state)
+
+
+    def getValue(self, state):
+        return self.computeValueFromQValues(state)
+
+
+class PacmanQAgent(QLearningAgent):
+    "Exactly the same as QLearningAgent, but with different default parameters"
+
+    def __init__(self, epsilon=0.05,gamma=0.8,alpha=0.2, numTraining=0, **args):
+        """
+        These default parameters can be changed from the pacman.py command line.
+        For example, to change the exploration rate, try:
+            python pacman.py -p PacmanQLearningAgent -a epsilon=0.1
+
+        alpha    - learning rate
+        epsilon  - exploration rate
+        gamma    - discount factor
+        numTraining - number of training episodes, i.e. no learning after these many episodes
+        """
+        args['epsilon'] = epsilon
+        args['gamma'] = gamma
+        args['alpha'] = alpha
+        args['numTraining'] = numTraining
+        self.index = 0  # This is always Pacman
+        QLearningAgent.__init__(self, **args)
+
+    def getAction(self, state):
+        """
+        Simply calls the getAction method of QLearningAgent and then
+        informs parent of action for Pacman.  Do not change or remove this
+        method.
+        """
+        action = QLearningAgent.getAction(self,state)
+        self.doAction(state,action)
+        return action
+
+
+class ApproximateQAgent(PacmanQAgent):
+    """
+       ApproximateQLearningAgent
+
+       You should only have to overwrite getQValue
+       and update.  All other QLearningAgent functions
+       should work as is.
+    """
+    def __init__(self, extractor='IdentityExtractor', **args):
+        self.featExtractor = util.lookup(extractor, globals())()
+        PacmanQAgent.__init__(self, **args)
+        self.weights = util.Counter()
+
+    def getWeights(self):
+        return self.weights
+
+    def getQValue(self, state, action):
+        """
+          Should return Q(state,action) = w * featureVector
+          where * is the dotProduct operator
+        """
+        "*** YOUR CODE HERE ***"
+        q_value = 0
+        features = self.featExtractor.getFeatures(state, action)
+
+        for feat in features:
+            q_value += features[feat] * self.weights[feat]
+
+        return q_value
+
+    def update(self, state, action, nextState, reward):
+        """
+           Should update your weights based on transition
+        """
+        "*** YOUR CODE HERE ***"
+        features = self.featExtractor.getFeatures(state, action)
+        q_value = self.getQValue(state, action)
+        diff = (reward + self.discount * self.getValue(nextState)) - q_value
+
+        for feat in features:
+            self.weights[feat] += self.alpha * diff * features[feat]
+
+    def final(self, state):
+        "Called at the end of each game."
+        # call the super-class final method
+        PacmanQAgent.final(self, state)
+
+        # did we finish training?
+        if self.episodesSoFar == self.numTraining:
+            # you might want to print your weights here for debugging
+            "*** YOUR CODE HERE ***"
+            pass
